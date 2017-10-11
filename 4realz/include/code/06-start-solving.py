520a0d9867e5adb207b447c89540c535a0c64149
numinit <me@numin.it>
2017-10-10 20:44:46 -0700

    add latest PDF with more slides

--- 4realz/include/code/06-start-solving.py
+++ 4realz/include/code/06-start-solving.py
@@ -1,8 +1,9 @@
 if smt.check() == z3.unsat:
     print('Sudoku is impossible to solve :(')
 else:
-    # Retrieve each cell from the model as a python long
     model = smt.model()
+
+    # Retrieve each cell from the model as a python long
     result = [list() for row in range(side)]
     for r in range(side):
         for c in range(side):
