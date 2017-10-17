f317a6d7626af6846271c11ce67ef1625a2dfb85
numinit <me@numin.it>
2017-10-16 20:34:18 -0700

    Fix some annoying things

--- 4realz/include/code/21-correct-constants.py
+++ 4realz/include/code/21-correct-constants.py
@@ -2,7 +2,7 @@ outputs = (
     0x02df028f,
     0x0290025d,
     0x02090221,
-    0x027b0278, # single bit typo
+    0x027b0278, # single bit typo on round 4!
     0x01f90233,
     0x025e0291,
     0x02290255,
